e8d6bf37c6f83d25ac0454afc2fb70f18e2955c2
Damon Davison <damon@allolex.net>
2014-01-21 16:10:41 -0500

    Add Connection#cancel
    - Changed Api::Response to handle optional and fewer fields
    - Tested both failure response and success for #cancel
    - Add cancellation path to Circle CI environment
    - Add cancel path environment variable to Travis
    - Remove redundant connection_v03 spec

--- lib/suretax/api/response.rb
+++ lib/suretax/api/response.rb
@@ -1,16 +1,20 @@
 module Suretax
   module Api
     class Response
-      attr_reader :status, :message, :total_tax, :groups, :transaction
+      attr_reader :status, :message, :total_tax, :groups, :transaction, :client_tracking
 
       def initialize(response_body)
         @status = response_body.fetch('ResponseCode')
         @transaction = response_body.fetch('TransId').to_s
         @message = response_body.fetch('HeaderMessage')
         @success = response_body.fetch('Successful') == 'Y'
+        @client_tracking = response_body['ClientTracking'] || nil
         @total_tax = Amount.new(response_body['TotalTax'])
-        @groups = response_body.fetch('GroupList').map do |group|
-          Group.new(group)
+        @groups = []
+        if response_body['GroupList'].respond_to?(:map)
+          @groups = response_body.fetch('GroupList').map do |group|
+            Group.new(group)
+          end
         end
       end
 
--- spec/lib/suretax/api/response_spec.rb
+++ spec/lib/suretax/api/response_spec.rb
@@ -4,84 +4,136 @@ describe Suretax::Api::Response do
 
   let(:api_response) { Suretax::Api::Response.new(response_body) }
 
-  context 'with a successful response' do
 
-    let(:response_body) { valid_test_response_body }
+  context 'for a normal post request' do
+    context 'with a successful response' do
+      let(:response_body) { valid_test_response_body }
 
-    it 'should return the API status code' do
-      expect(api_response.status).to eql('9999')
-    end
-
-    it 'should be successful' do
-      expect(api_response).to be_success
-    end
-
-    it 'should have a message of "Success"' do
-      expect(api_response.message).to eql('Success')
-    end
-
-    it 'should have the correct total tax' do
-      expect(api_response.total_tax.to_f).to eql(1.394490)
-    end
-
-    it 'should have a transaction id' do
-      expect(api_response.transaction).to eql('2664495')
-    end
-
-    context 'invoice groups' do
-      it 'should be the correct number' do
-        expect(api_response.groups.count).to eql(1)
-      end
-
-      it 'should respond to #invoice' do
-        expect(api_response.groups.first).to respond_to(:invoice)
-      end
-    end
-  end
-
-  context 'with a partially successful response' do
-
-    let(:response_body) { success_with_item_errors }
-
-    describe '#status' do
       it 'should return the API status code' do
-        expect(api_response.status).to eql('9001')
+        expect(api_response.status).to eql('9999')
       end
-    end
 
-    describe '#success?' do
-      it 'should be true' do
+      it 'should be successful' do
         expect(api_response).to be_success
       end
+
+      it 'should have a message of "Success"' do
+        expect(api_response.message).to eql('Success')
+      end
+
+      it 'should have the correct total tax' do
+        expect(api_response.total_tax.to_f).to eql(1.394490)
+      end
+
+      it 'should have a transaction id' do
+        expect(api_response.transaction).to eql('2664495')
+      end
+
+      context 'invoice groups' do
+        it 'should be the correct number' do
+          expect(api_response.groups.count).to eql(1)
+        end
+
+        it 'should respond to #invoice' do
+          expect(api_response.groups.first).to respond_to(:invoice)
+        end
+      end
     end
 
-    describe '#message' do
-      it 'should start with "Failure"' do
-        expect(api_response.message).to match(/\ASuccess with item errors/i)
+    context 'with a partially successful response' do
+      let(:response_body) { success_with_item_errors }
+
+      describe '#status' do
+        it 'should return the API status code' do
+          expect(api_response.status).to eql('9001')
+        end
+      end
+
+      describe '#success?' do
+        it 'should be true' do
+          expect(api_response).to be_success
+        end
+      end
+
+      describe '#message' do
+        it 'should start with "Failure"' do
+          expect(api_response.message).to match(/\ASuccess with item errors/i)
+        end
+      end
+    end
+
+    context 'with a failure response' do
+      let(:response_body) { valid_failure_response }
+
+      describe '#status' do
+        it 'should return the API status code' do
+          expect(api_response.status).to eql('1101')
+        end
+      end
+
+      describe '#success?' do
+        it 'should be false' do
+          expect(api_response.success?).to be_false
+        end
+      end
+
+      describe '#message' do
+        it 'should start with "Failure"' do
+          expect(api_response.message).to match(/\AFailure/)
+        end
       end
     end
   end
 
-  context 'with a failure response' do
+  context 'for a cancel post request' do
+    context 'with a successful response' do
+      let(:response_body) { cancel_response_body }
 
-    let(:response_body) { valid_failure_response }
-
-    describe '#status' do
       it 'should return the API status code' do
-        expect(api_response.status).to eql('1101')
+        expect(api_response.status).to eql('9999')
+      end
+
+      it 'should be successful' do
+        expect(api_response).to be_success
+      end
+
+      it 'should have a message of "Success"' do
+        expect(api_response.message).to eql('Success')
+      end
+
+      it 'should have a transaction id' do
+        expect(api_response.transaction).to eql('0')
+      end
+
+      it 'should have the client tracking code set' do
+        expect(api_response.client_tracking).to eql('test')
       end
     end
 
-    describe '#success?' do
-      it 'should be false' do
-        expect(api_response.success?).to be_false
-      end
-    end
+    context 'with a failed request' do
 
-    describe '#message' do
-      it 'should start with "Failure"' do
-        expect(api_response.message).to match(/\AFailure/)
+      let(:response_body) { cancel_failed_response_body }
+
+      it 'should return the API status code' do
+        expect(api_response.status).to be_nil
+      end
+
+      it 'should be successful' do
+        expect(api_response).to_not be_success
+      end
+
+      it 'should have a message of "Success"' do
+        expect(api_response.message).to be_nil
+      end
+
+      it 'should have a transaction id' do
+        expect(api_response.transaction).to eql('0')
+      end
+
+      it 'should have the client tracking code set' do
+        expect(api_response.client_tracking).to be_nil
       end
     end
   end
+
 end
