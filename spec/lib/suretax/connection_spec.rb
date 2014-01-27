854a41563a4b97254411fae20477e3e390f46772
Damon Davison <damon@allolex.net>
2014-01-27 12:30:27 -0500

    Move Connection#post and #cancel fully into shared examples - Use do…end or single line let statements with curly braces

--- spec/lib/suretax/connection_spec.rb
+++ spec/lib/suretax/connection_spec.rb
@@ -4,21 +4,37 @@ describe Suretax::Connection do
 
   let(:connection) { Suretax::Connection.new }
 
-  let(:request_body) {
-    valid_encoded_test_request_body
-  }
-
-  let(:response) {
-    connection.post(body: request_body)
-  }
-
   context 'using v01 of the API' do
 
     describe '#post' do
       it_should_behave_like 'API connection' do
-        let(:response_body) {
+        let(:api_path) { suretax_post_path }
+        let(:request_body) { valid_encoded_test_request_body }
+        let(:response) { connection.post(body: request_body) }
+
+        let(:response_body) do
           suretax_wrap_response(valid_test_response_body.to_json)
-        }
+        end
+
+        let(:invalid_response_body) do
+          suretax_wrap_response(post_failed_response_body.to_json)
+        end
+      end
+    end
+
+    describe '#cancel' do
+      it_should_behave_like 'API connection' do
+        let(:api_path) { suretax_cancel_path }
+        let(:request_body) { cancel_request_body }
+        let(:response) { connection.cancel(body: request_body) }
+
+        let(:response_body) do
+          suretax_wrap_response(cancel_response_body.to_json)
+        end
+
+        let(:invalid_response_body) do
+          suretax_wrap_response(cancel_failed_response_body.to_json)
+        end
       end
     end
 
@@ -28,11 +44,34 @@ describe Suretax::Connection do
 
     describe '#post' do
       it_should_behave_like 'API connection' do
-        let(:response_body) {
+        let(:api_path) { suretax_post_path }
+        let(:request_body) { valid_encoded_test_request_body }
+        let(:response) { connection.post(body: request_body) }
+
+        let(:response_body) do
           suretax_wrap_response(valid_v03_response_body.to_json)
-        }
+        end
+
+        let(:invalid_response_body) do
+          suretax_wrap_response(post_failed_response_body.to_json)
+        end
       end
     end
 
+    describe '#cancel' do
+      it_should_behave_like 'API connection' do
+        let(:api_path) { suretax_cancel_path }
+        let(:request_body) { cancel_request_body }
+        let(:response) { connection.cancel(body: request_body) }
+
+        let(:response_body) do
+          suretax_wrap_response(cancel_response_body.to_json)
+        end
+
+        let(:invalid_response_body) do
+          suretax_wrap_response(cancel_failed_response_body.to_json)
+        end
+      end
+    end
   end
 end
--- spec/support/connection_shared_examples.rb
+++ spec/support/connection_shared_examples.rb
@@ -1,19 +1,37 @@
 require 'spec_helper'
 
 shared_examples_for 'API connection' do
-  before do
-    stub_request(:post, "#{suretax_url}#{suretax_post_path}").to_return(
-      status: 200,
-      body: response_body
-    )
+  context 'with a valid request' do
+    before do
+      stub_request(:post, "#{suretax_url}#{api_path}").to_return(
+        status: 200,
+        body: response_body
+      )
+    end
+
+    it 'should be successful' do
+      expect(response).to be_success
+    end
+
+    it 'should have a urlencode header' do
+      expect(connection.headers['Content-Type']).to include('application/x-www-form-urlencoded')
+    end
   end
 
-  it 'should be successful' do
-    expect(response).to be_success
-  end
+  context 'with an invalid request' do
 
-  it 'should have a urlencode header' do
-    expect(connection.headers['Content-Type']).to include('application/x-www-form-urlencoded')
+    let(:request_body) { {} }
+
+    before do
+      stub_request(:post, "#{suretax_url}#{api_path}").to_return(
+        status: 500,
+        body: invalid_response_body
+      )
+    end
+
+    it 'should not be successful' do
+      expect(response).to_not be_success
+    end
   end
 end
 
